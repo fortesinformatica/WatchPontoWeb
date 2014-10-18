a578487c3ed27de3b88ef69de0f22b68407a2c3b
Joel R Michaliszen <joel.r.mz@gmail.com>
2014-10-18 16:52:25 -0300

    Lancamento Manual DS & VM

---
+++ WatchPontoWeb/Scripts/viewmodels/lancamentoViewModel.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    var lancamentoManualViewModel = {
+        PIS: ko.observable(),
+        DtRegistro: ko.observable(),
+        Tipo: ko.observable(),
+        Justificativa: ko.observable()
+    }
+
+    ko.applyBindings(lancamentoManualViewModel, $(".lancamento-manual")[0]);
+
+    $("form").submit(function () {
+
+        var loginDs = new LoginDataSource();
+        loginDs.login(ko.toJS(loginViewModel));
+
+        return false;
+    });
+});
