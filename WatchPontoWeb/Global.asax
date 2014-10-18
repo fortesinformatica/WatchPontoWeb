c5619e0de8792ca9e69080d596fa8d633072e0f6
Gean Júnior <gean.adm@gmail.com>
2014-10-18 11:44:17 -0300

    Commit inicial.

---
+++ WatchPontoWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WatchPontoWeb.MvcApplication" Language="C#" %>
---
+++ WatchPontoWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WatchPontoWeb.Startup))]
+namespace WatchPontoWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
